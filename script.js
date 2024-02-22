e120ab95f4cddaa40258fc4d09d08629559c4ba3
MANDIE <93381512+kirayoshikage101@users.noreply.github.com>
2024-02-22 05:47:53 -0800

    new changes again

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
 
-const myName = document.getElementsByClassName("myname");
-const paragraph = document.getElementsByClassName("dev");
+const myName = document.getElementsByClassName("myname")[0];
+const paragraph = document.getElementsByClassName("dev")[0];
 const text1 = 'Hello, I am Mandie Limbaga!';
 const text2 = "Front-End Web Developer from Philippines.";
 
 const textTypeWriter = (element, text, i = 0) => {
-    element.innerText += text;
-
+    element.textContent += text[i]
     if(i === text.length - 1){
         return;
     }
