ccde0377f566f4b1dad7905524f3ec62ae3e7009
MANDIE <93381512+kirayoshikage101@users.noreply.github.com>
2024-02-22 05:59:55 -0800

    new changes again

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const text1 = 'Hello, I am Mandie Limbaga!';
 const text2 = "Front-End Web Developer from Philippines.";
 
 const textTypeWriter = (element, text, i = 0) => {
-    element.textContent += text[i]
+    element.textContent = text.substring(9, i +1);
+
     if(i === text.length - 1){
         return;
     }
 
-    setTimeout(() => textTypeWriter(element, text, i , + 1), 50)
+    setTimeout(() => textTypeWriter(element, text, i , + 1), 50);
 }
 
 textTypeWriter(myName, text1);
